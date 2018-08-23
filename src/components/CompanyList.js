94b52c93932c54fda3648766e6fa905a45a06e9d
Seho Lim <limseho657424@gmail.com>
2018-08-22 21:31:10 -0500

    Deleted the custom Header component from CompanyList to solve conflicts with react-native-router-flux

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -2,25 +2,23 @@ import React, { Component } from 'react';
 import { ScrollView, View } from 'react-native';
 import CompanyDetail from './CompanyDetail';
 import company from './company.json';
-import Header from './commons';
 
 class CompanyList extends Component {
   constructor() {
     super();
     this.state = { companies: company };
   }
-
+  
   renderCompanies() {
+    console.log('render company');
     return this.state.companies.map(c => <CompanyDetail key={c.id} company={c} />);
   }
 
   render() {
     //console.log(this.state);
-
+    console.log('render');
     return (
       <View style={{ flex: 1 }}>
-        <Header headerText={'Company List'} />
-        <CompanyList />
         <ScrollView>{this.renderCompanies()}</ScrollView>
       </View>
     );
