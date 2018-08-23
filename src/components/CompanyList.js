4372f50408a19d82b5e6e92569288a87be52447b
junlee91 <jlee757@uic.edu>
2018-08-22 20:35:20 -0500

    constructor in React component

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -5,27 +5,26 @@ import company from './company.json';
 import Header from './commons';
 
 class CompanyList extends Component {
-    state = { companies: company };
+  constructor() {
+    super();
+    this.state = { companies: company };
+  }
 
-    renderCompanies() {    
-        return this.state.companies.map(c => 
-            <CompanyDetail key={c.id} company={c} />
-        );
-    }
+  renderCompanies() {
+    return this.state.companies.map(c => <CompanyDetail key={c.id} company={c} />);
+  }
 
-    render() {
-        //console.log(this.state);
-        
-        return (
-            <View style={{ flex: 1 }}>
-                <Header headerText={'Company List'} />
-                <CompanyList />
-                <ScrollView>
-                    {this.renderCompanies()}
-                </ScrollView>
-            </View>
-        );
-    }
+  render() {
+    //console.log(this.state);
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={'Company List'} />
+        <CompanyList />
+        <ScrollView>{this.renderCompanies()}</ScrollView>
+      </View>
+    );
+  }
 }
 
 export default CompanyList;
