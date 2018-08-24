4d68772752fbd9f7df93707115edf87a634dd447
junlee91 <jlee757@uic.edu>
2018-08-23 20:25:18 -0500

    CompanyList setting + Main spinner

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, StyleSheet } from 'react-native';
+
 import CompanyDetail from './CompanyDetail';
+import { Spinner } from './commons';
 import company from './company.json';
 
 class CompanyList extends Component {
   constructor() {
     super();
-    this.state = { companies: company };
+    this.state = {
+      loading: true
+    };
   }
-  
+
+  componentDidMount() {
+    // this is where we call API
+
+    setTimeout(() => {
+      this.setState({
+        companies: company,
+        loading: false
+      });
+    }, 3000); // wait 3 seconds for fetching the data
+  }
+
+  componentWillReceiveProps = nextProps => {
+    // here we have company data
+    if (nextProps.company) {
+      console.log('this is not yet implemented');
+
+      this.setState({
+        loading: false
+      });
+    }
+  };
+
   renderCompanies() {
-    console.log('render company');
     return this.state.companies.map(c => <CompanyDetail key={c.id} company={c} />);
   }
 
   render() {
-    //console.log(this.state);
-    console.log('render');
     return (
-      <View style={{ flex: 1 }}>
-        <ScrollView>{this.renderCompanies()}</ScrollView>
+      <View style={styles.container}>
+        {this.state.loading ? (
+          <Spinner />
+        ) : (
+          <ScrollView>{this.renderCompanies()}</ScrollView>
+        )}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default CompanyList;
--- src/components/commons/Spinner.js
+++ src/components/commons/Spinner.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
 
+// This Spinner take all screen
 const Spinner = ({ size }) => (
-    <View style={styles.spinnerStyle}>
-        <ActivityIndicator size={size || 'large'} />
-    </View>
+  <View style={[styles.container, styles.horizontal]}>
+    <ActivityIndicator size={size || 'large'} />
+  </View>
 );
 
-const styles = {
-    spinnerStyle: {
-        flex: 1,
-        justifuContent: 'center',
-        alignItems: 'center'
-    }
-};
+// More customized spinner goes here..
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+});
 
 export { Spinner };
